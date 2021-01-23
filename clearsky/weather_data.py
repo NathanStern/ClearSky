3dfe700806dc84c754ced20725424d125f45fa47
Isabella Stookey <istookey@purdue.edu>
2021-01-23 14:38:36 -0500

    added constellation map

--- clearsky/weather_data.py
+++ clearsky/weather_data.py
@@ -2,8 +2,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from flask.signals import request_started
+import requests
+import json
 
 bp = Blueprint('weather-data', __name__, url_prefix='/weather_data')
+
+url_format = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=minutely,hourly,daily,alerts&appid=536159a5b4c02240e63a25443055734b"
 
 @bp.route('/')
 def test():
@@ -11,4 +15,6 @@ def test():
 
 @bp.route('/lat=<float(signed=true):lat>/lon=<float(signed=true):lon>', methods=('GET', 'POST'))
 def display_data(lat, lon):
-    return "lat = {}, lon = {}".format(lat, lon)
+    r = requests.get(url_format.format(lat, lon))
+    return render_template('weatherpage/weather_data.html', json = r.json(), url="http://slowe.github.io/VirtualSky/embed?longitude={}&latitude={}&projection=polar&constellations=true&constellationlabels=true&showstarlabels=true".format(lon, lat))
+    # return "lat = {}, lon = {}".format(lat, lon)
