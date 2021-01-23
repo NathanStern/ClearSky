e1ec9b35c14f1a606fcf94b0e89bf56b2e4f4d92
Nathan <nstern@wearelcc.ca>
2021-01-22 23:27:03 -0500

    changed hello app route

--- clearsky/__init__.py
+++ clearsky/__init__.py
@@ -3,6 +3,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -27,7 +28,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that just says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return "Hello, World!"
 
