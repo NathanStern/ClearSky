d6e2b76d20e6cb071230d78762e36cecd27fc87c
Nathan <nstern@wearelcc.ca>
2021-01-23 23:56:03 -0500

    fixes

--- clearsky/main.py
+++ clearsky/main.py
@@ -8,6 +8,7 @@ from . import db
 app = create_app()
 
 with app.app_context():
+
     if not os.path.exists(current_app.instance_path.join('clearsky.sqlite')):
         print("Initializing database for first-time use")
         db.init_db()
